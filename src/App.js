9a1a1e5f3e9a6f731358d6e1cd4b39e21e7bc421
vitoraok1 <vitoraoki17@gmail.com>
2023-02-09 16:37:36 -0300

    cria o botão de iniciar o jogo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import GamePage from './pages/GamePage';
 import './App.css';
 
 export default function App() {
   return (
     <div>
       <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ GamePage } />
     </div>
   );
 }
