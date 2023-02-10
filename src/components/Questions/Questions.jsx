1202faa448b1187a2a8971e6a028e2ad94ab0308
vitoraok1 <vitoraoki17@gmail.com>
2023-02-10 10:19:33 -0300

    adiciona contador

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -7,32 +7,64 @@ class Questions extends Component {
   state = {
     playerAnswer: 'hide',
     quiz: [],
+    counter: 30,
+    showAnswers: false,
   };
 
   componentDidMount() {
-    const { questions, curr } = this.props;
-    const quiz = [questions[curr].correct_answer, ...questions[curr].incorrect_answers];
-    this.setState({
-      quiz: this.shuffle(quiz),
-    });
+    this.shuffleQuestions();
+    this.timeCounter();
   }
 
-  shuffle = (array) => {
+  shuffleQuestions = () => {
+    const { questions, curr } = this.props;
     const rand = 0.5;
-    return array.sort(() => (
+    const quiz = [questions[curr].correct_answer, ...questions[curr].incorrect_answers];
+    const randomQuiz = quiz.sort(() => (
       Math.random() - rand
     ));
+
+    this.setState({
+      quiz: randomQuiz,
+    });
+  };
+
+  timeCounter = () => {
+    this.setState({ counter: 30 }, () => {
+      const second = 1000;
+      const interval = setInterval(() => {
+        this.setState((prevState) => ({
+          counter: prevState.counter - 1,
+        }), () => {
+          const { counter, showAnswers } = this.state;
+          if (counter === 0 || showAnswers) {
+            clearInterval(interval);
+            this.setState({ showAnswers: true, playerAnswer: 'show' });
+          }
+        });
+      }, second);
+    });
   };
 
   handleAnswer = () => {
-    this.setState({
-      playerAnswer: 'show',
-    });
+    const { playerAnswer, showAnswers } = this.state;
+
+    if (playerAnswer === 'show') {
+      this.setState({
+        showAnswers: !showAnswers,
+        playerAnswer: 'hide',
+      });
+    } else {
+      this.setState({
+        showAnswers,
+        playerAnswer: 'show',
+      });
+    }
   };
 
   render() {
     const { questions, curr } = this.props;
-    const { quiz, playerAnswer } = this.state;
+    const { quiz, playerAnswer, showAnswers, counter } = this.state;
     const { category, question } = questions[curr];
 
     return (
@@ -51,10 +83,19 @@ class Questions extends Component {
                 `${playerAnswer}${questions[curr]
                   .correct_answer === answer ? 'Correct' : 'Wrong'}`
               }
+              disabled={ showAnswers || counter === 0 }
             >
               { answer }
             </button>
           ))}
+          <br />
+          <br />
+          <span>
+            {' '}
+            Time left:
+            {' '}
+            { counter }
+          </span>
         </div>
       </div>
     );
