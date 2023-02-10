864683e7875d0a2cf17e125bba4488e5db633ccc
vitoraok1 <vitoraoki17@gmail.com>
2023-02-10 16:59:55 -0300

    merge fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { string } from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { name, score } = this.props;
+    const { name, score, url } = this.props;
 
     return (
       <header>
-        <img data-testid="header-profile-picture" alt={ name } />
+        <img data-testid="header-profile-picture" alt={ name } src={ url } />
         <p data-testid="header-player-name">{ name }</p>
         <p data-testid="header-score">{ score }</p>
       </header>
@@ -16,13 +16,16 @@ class Header extends Component {
   }
 }
 
-const mapStateToProps = (globalState) => ({
-  name: globalState.player.name,
-  score: globalState.player.score,
+const mapStateToProps = (state) => ({
+  name: state.player.name,
+  score: state.player.score,
+  url: state.player.url,
 });
 
 Header.propTypes = {
-  name: string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  url: PropTypes.string,
 }.isRequired;
 
 export default connect(mapStateToProps)(Header);
