0d437cd85976b799c2450d748e6746e01758ccf2
Victor Massini <vmprata@gmail.com>
2023-02-10 16:32:08 -0300

    Testando os requisitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { string } from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { name, email, url } = this.props;
+    const { name, score } = this.props;
+
     return (
-      <div>
+      <header>
+        <img data-testid="header-profile-picture" alt={ name } />
         <p data-testid="header-player-name">{ name }</p>
-        <img src={ url } alt={ email } data-testid="header-profile-picture" />
-        <p data-testid="header-score">0</p>
-      </div>
+        <p data-testid="header-score">{ score }</p>
+      </header>
     );
   }
 }
-const mapStateToProps = (state) => ({
-  name: state.user.name,
-  email: state.user.email,
-  url: state.user.url,
+
+const mapStateToProps = (globalState) => ({
+  name: globalState.player.name,
+  score: globalState.player.score,
 });
 
 Header.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-};
+  name: string,
+}.isRequired;
 
 export default connect(mapStateToProps)(Header);
